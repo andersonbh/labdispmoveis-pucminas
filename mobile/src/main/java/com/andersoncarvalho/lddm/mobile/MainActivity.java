01d301ad2271c75874fa8a7539ff29d89bf47133
Anderson Carvalho <andersonbdcarvalho@gmail.com>
2015-03-03 14:08:23 -0300

    Versao final totalmente funcional

--- mobile/src/main/java/com/andersoncarvalho/lddm/mobile/MainActivity.java
+++ mobile/src/main/java/com/andersoncarvalho/lddm/mobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andersoncarvalho.lddm.mobile;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -20,9 +21,12 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
 
     private ArrayList<Map<String, String>> listaContatos;
     private SimpleAdapter mAdapter;
-    private AutoCompleteTextView numTel;
-    private Button ligar;
+    private AutoCompleteTextView autoComplete;
+    private String Ident;
+    private Button ligar, mandarEmail, mapa;
     String numero;
+    String [] emailContato;
+    String endereco;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,23 +34,35 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
         setContentView(R.layout.activity_main);
         listaContatos = new ArrayList<Map<String, String>>();
         montarLista();
-        numTel = (AutoCompleteTextView) findViewById(R.id.mmWhoNo);
-        ligar=(Button) findViewById(R.id.button1);
+        autoComplete = (AutoCompleteTextView) findViewById(R.id.mmWhoNo);
+        ligar=(Button) findViewById(R.id.ligar);
+        mandarEmail=(Button) findViewById(R.id.email);
+        mandarEmail.setEnabled(false);
+        mapa=(Button) findViewById(R.id.mapa);
+        mapa.setEnabled(false);
         mAdapter = new SimpleAdapter(this, listaContatos, R.layout.custcontview,
-                new String[] { "Name", "Phone", "Type" }, new int[] {
-                R.id.ccontName, R.id.ccontNo, R.id.ccontType });
-        numTel.setAdapter(mAdapter);
-
-        numTel.setOnItemClickListener(new OnItemClickListener() {
+                new String[] { "Nome", "Telefone" }, new int[] {
+                R.id.ccontName, R.id.ccontNo });
+        autoComplete.setAdapter(mAdapter);
+         Ident = "";
+        endereco = "";
+        autoComplete.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> av, View arg1, int index,
                                     long arg3) {
                 Map<String, String> map = (Map<String, String>) av.getItemAtPosition(index);
 
-                String name  = map.get("Name");
-                numero = map.get("Phone");
-                numTel.setText(name+" " +numero);
+                String name  = map.get("Nome");
+                numero = map.get("Telefone");
+                Ident = map.get("Id");
+                autoComplete.setText(name+" " +numero);
+                emailContato = null;
+                endereco = "";
+                lerDadosContato(Ident);
+                
+                
+
             }
         });
 
@@ -54,13 +70,48 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
             public void onClick(View v) {
                 //Toast.makeText(MainActivity.this, numero.toString(), Toast.LENGTH_SHORT).show();
                 try {
-                    if (numTel != null) {
+                    if (autoComplete != null) {
                         startActivity(new Intent(Intent.ACTION_CALL, Uri
                                 .parse("tel:" +numero)));
-                    }else if(numTel != null && numero.toString().length()==0){
+                    }else if(autoComplete != null && numero.toString().length()==0){
                         Toast.makeText(getApplicationContext(), "You missed to type the numero!", Toast.LENGTH_SHORT).show();
-                    }else if(numTel != null && numTel.getText().length()<10){
+                    }else if(autoComplete != null && autoComplete.getText().length()<10){
                         Toast.makeText(getApplicationContext(), "Check whether you entered correct numero!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    Log.e("DialerAppActivity", "error: " + e.getMessage(),
+                            e);
+                }
+            }
+        });
+        mandarEmail.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    if (autoComplete != null) {
+                        if(emailContato != null) {
+                            Intent intent = new Intent(Intent.ACTION_SENDTO);
+                            intent.setData(Uri.parse("mailto:"));
+                            intent.putExtra(Intent.EXTRA_EMAIL, emailContato);
+                            Log.d("Tentando enviar email para", emailContato[0]);
+                            if (intent.resolveActivity(getPackageManager()) != null) {
+                                startActivity(intent);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e("DialerAppActivity", "error: " + e.getMessage(),
+                            e);
+                }
+            }
+        });
+        mapa.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(endereco));
+                    Log.d("Tentando ir para o endereco", endereco);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
                     }
                 } catch (Exception e) {
                     Log.e("DialerAppActivity", "error: " + e.getMessage(),
@@ -84,51 +135,153 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
                             .getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
 
             if ((Integer.parseInt(hasPhone) > 0)){
-                // You know have the numero so now query it like this
                 Cursor phones = getContentResolver().query(
                         ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                         null,
                         ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ contactId,
                         null, null);
                 while (phones.moveToNext()){
-                    //store numeros and display a dialog letting the user select which.
                     String phonenumero = phones.getString(
                             phones.getColumnIndex(
                                     ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    String numeroType = phones.getString(phones.getColumnIndex(
-                            ContactsContract.CommonDataKinds.Phone.TYPE));
                     Map<String, String> NamePhoneType = new HashMap<String, String>();
-                    NamePhoneType.put("Name", contactName);
-                    NamePhoneType.put("Phone", phonenumero);
-                    if(numeroType.equals("0"))
-                        NamePhoneType.put("Type", "Work");
-                    else
-                    if(numeroType.equals("1"))
-                        NamePhoneType.put("Type", "Home");
-                    else if(numeroType.equals("2"))
-                        NamePhoneType.put("Type",  "Mobile");
-                    else
-                        NamePhoneType.put("Type", "Other");
-                    //Then add this map to the list.
+                    NamePhoneType.put("Nome", contactName);
+                    NamePhoneType.put("Telefone", phonenumero);
+                    NamePhoneType.put("Id", contactId);
                     listaContatos.add(NamePhoneType);
                 }
                 phones.close();
             }
         }
         people.close();
-        startManagingCursor(people);
+//        startManagingCursor(people);
     }
 
 
-/*public void onItemClick(AdapterView<?> av, View v, int index, long arg){
-    Map<String, String> map = (Map<String, String>) av.getItemAtPosition(index);
-    Iterator<String> myVeryOwnIterator = map.keySet().iterator();
-    while(myVeryOwnIterator.hasNext()) {
-        String key=(String)myVeryOwnIterator.next();
-        String value=(String)map.get(key);
-        numTel.setText(value);
+    public void lerDadosContato(String ident) {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                if (ident.equals(cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID)))) {
+                    String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                        System.out.println("name : " + name + ", ID : " + id);
+                        Log.d("NOME", name);
+
+                        // Pegar numero do telefone
+                        Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            String phone = pCur.getString(
+                                    pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            System.out.println("phone" + phone);
+                        }
+                        pCur.close();
+
+
+                        // pegar email e tipo
+
+                        Cursor emailCur = cr.query(
+                                ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                        mandarEmail.setEnabled(false);
+                        while (emailCur.moveToNext()) {
+                            // This would allow you get several email addresses
+                            // if the email addresses were stored in an array
+                            String email = emailCur.getString(
+                                    emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                            String emailType = emailCur.getString(
+                                    emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE));
+                            mandarEmail.setEnabled(true);
+                            if(emailContato == null) {
+                                emailContato = new String[] { email };
+                                System.out.println("Email " + email + " Email Type : " + emailType);
+                            }
+                        }
+                        emailCur.close();
+
+                        // Pegar observacoes
+                        String noteWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                        String[] noteWhereParams = new String[]{id,
+                                ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE};
+                        Cursor noteCur = cr.query(ContactsContract.Data.CONTENT_URI, null, noteWhere, noteWhereParams, null);
+                        if (noteCur.moveToFirst()) {
+                            String note = noteCur.getString(noteCur.getColumnIndex(ContactsContract.CommonDataKinds.Note.NOTE));
+                            System.out.println("Note " + note);
+                        }
+                        noteCur.close();
+
+                        // Pegar endereço
+
+                        String addrWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                        String[] addrWhereParams = new String[]{id,
+                                ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE};
+                        Cursor addrCur = cr.query(ContactsContract.Data.CONTENT_URI,
+                                null, null, null, null);
+                        mapa.setEnabled(false);
+                        while (addrCur.moveToNext()) {
+                            String poBox = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POBOX));
+                            String street = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
+                            String city = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
+                            String state = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.REGION));
+                            String postalCode = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE));
+                            String country = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY));
+                            String type = addrCur.getString(
+                                    addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.TYPE));
+                            if(street != null && city != null && state != null) {
+                                mapa.setEnabled(true);
+                                endereco = "geo:0,0?q=" + street + "+" + city + "+" + state;
+                            }
+                            // Do something with these....
+
+                        }
+                        addrCur.close();
+
+                        // Get Instant Messenger.........
+                        String imWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                        String[] imWhereParams = new String[]{id,
+                                ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE};
+                        Cursor imCur = cr.query(ContactsContract.Data.CONTENT_URI,
+                                null, imWhere, imWhereParams, null);
+                        if (imCur.moveToFirst()) {
+                            String imName = imCur.getString(
+                                    imCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.DATA));
+                            String imType;
+                            imType = imCur.getString(
+                                    imCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.TYPE));
+                        }
+                        imCur.close();
+
+                        // Get Organizations.........
+
+                        String orgWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                        String[] orgWhereParams = new String[]{id,
+                                ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE};
+                        Cursor orgCur = cr.query(ContactsContract.Data.CONTENT_URI,
+                                null, orgWhere, orgWhereParams, null);
+                        if (orgCur.moveToFirst()) {
+                            String orgName = orgCur.getString(orgCur.getColumnIndex(ContactsContract.CommonDataKinds.Organization.DATA));
+                            String title = orgCur.getString(orgCur.getColumnIndex(ContactsContract.CommonDataKinds.Organization.TITLE));
+                        }
+                        orgCur.close();
+                    }
+                }
+            }
+        }
     }
-}*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
