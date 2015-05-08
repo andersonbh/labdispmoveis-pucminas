5cefa1d801848899c9ef7636a5a7062e6ce21f12
Anderson Carvalho <andersonbdcarvalho@gmail.com>
2015-05-08 18:30:17 -0300

    tp03 inicial release

--- mobile/src/main/java/com/andersoncarvalho/lddm/mobile/MainActivity.java
+++ mobile/src/main/java/com/andersoncarvalho/lddm/mobile/MainActivity.java
@@ -1,81 +1,30 @@
 package com.andersoncarvalho.lddm.mobile;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.*;
-import android.widget.*;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-
-    static String url = "https://twitter.com/search?q=%23lavajato&src=typd";
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        String[] partidos = getResources().getStringArray(R.array.partidos_array);
-        mTitle = partidos[number - 1];
-    }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, new MapaFragment())
+                    .commit();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
@@ -85,116 +34,11 @@ public class MainActivity extends ActionBarActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-//        //noinspection SimplifiableIfStatement
+        //noinspection SimplifiableIfStatement
 //        if (id == R.id.action_settings) {
 //            return true;
 //        }
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-        ListView lista;
-        ImageView imagem;
-        ImageButton btn_twitter;
-        String[] dados;
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container,
-                    false);
-            int i = getArguments().getInt(ARG_SECTION_NUMBER);
-            lista = (ListView) rootView.findViewById(R.id.politicos_partido);
-            imagem = (ImageView) rootView.findViewById(R.id.imageView1);
-            btn_twitter = (ImageButton) rootView.findViewById(R.id.imageButton);
-            dados = new String[1];
-
-            btn_twitter.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Uri uri = Uri.parse(url);
-                    Intent webintent = new Intent(Intent.ACTION_VIEW,uri);
-                    startActivity(webintent);
-                }
-            });
-
-            setarListadePartidos(i);
-            return rootView;
-        }
-
-
-        public void setarListadePartidos(int i) {
-            lista.setVisibility(View.VISIBLE);
-            lista.setActivated(true);
-
-            switch (i) {
-                case 1: {
-                    dados = getResources().getStringArray(R.array.PMDB);
-                    imagem.setImageDrawable(getResources().getDrawable(R.drawable.pmdb));
-                    break;
-                }
-                case 2: {
-                    dados = getResources().getStringArray(R.array.PP);
-                    imagem.setImageDrawable(getResources().getDrawable(R.drawable.pp));
-                    break;
-                }
-                case 3: {
-                    dados = getResources().getStringArray(R.array.PSDB);
-                    imagem.setImageDrawable(getResources().getDrawable(R.drawable.psdb));
-                    break;
-                }
-                case 4: {
-                    dados = getResources().getStringArray(R.array.PTB);
-                    imagem.setImageDrawable(getResources().getDrawable(R.drawable.ptb));
-                    break;
-                }
-                case 5: {
-                    dados = getResources().getStringArray(R.array.PT);
-                    imagem.setImageDrawable(getResources().getDrawable(R.drawable.pt));
-                    break;
-                }
-                case 6: {
-                    dados = getResources().getStringArray(R.array.Apartidario);
-                    imagem.setImageDrawable(getResources().getDrawable(R.drawable.republicabrasil));
-                    break;
-                }
-            }
-
-            if (lista.isActivated()) {
-                lista.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, dados));
-            }
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
---
+++ mobile/src/main/java/com/andersoncarvalho/lddm/mobile/MapaFragment.java
@@ -0,0 +1,309 @@
+package com.andersoncarvalho.lddm.mobile;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
+/**
+ * Created by anderson on 08/05/15.
+ */
+
+public class MapaFragment extends Fragment implements View.OnClickListener, ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener {
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number", LOG = "LDDM";
+
+    public GoogleMap mMap;
+    MapView mMapView;
+    int ZOOM = 18, BEARING, opcaoFragment;
+    private CameraPosition cp;
+    private static View v;
+    LatLng posicao;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    Marker minhaloc;
+    Location localizacao;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        callConnection();
+        opcaoFragment = getArguments().getInt(ARG_SECTION_NUMBER);
+        if (v != null) {
+            ViewGroup parent = (ViewGroup) v.getParent();
+            if (parent != null) {
+                parent.removeView(v);
+            }
+        }
+        try {
+            v = inflater.inflate(R.layout.mapa_fragment, container, false);
+        } catch (InflateException e) {
+        }
+        //Inicia o MapView para dar suporte ao mapa google maps
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMap = mMapView.getMap();
+        //Monta o mapa personalizado.
+        montarMapa();
+        return v;
+    }
+
+    private void getLocalizacao() {
+        if (localizacao != null) {
+            LatLng latLng = new LatLng(localizacao.getLatitude(), localizacao.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18);
+            mMap.animateCamera(cameraUpdate);
+        }else{
+           Log.d(LOG,"sem localização");
+        }
+    }
+
+    public void zoomTop(LatLng posicao, int zoom) {
+        if (cp == null) {
+            cp = new CameraPosition.Builder().target(posicao).zoom(zoom).bearing(BEARING).tilt(0).build();
+        }
+        CameraUpdate update = CameraUpdateFactory.newCameraPosition(cp);
+        mMap.animateCamera(update, 2000, null);
+        mMap.moveCamera(update);
+        cp = null;
+
+        mMap.setBuildingsEnabled(true);
+    }
+
+    public static Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+    public void montarMapa() {
+            if (mMap != null) {
+                //limpar markers do mapa na troca de fragment
+                limparMarcadores();
+                //Habilitar localizacao do usuario - desabilitado pois utiliza botao customizado
+                mMap.setMyLocationEnabled(true);
+                // Setar controles de zoom in e zoom out
+                mMap.getUiSettings().setZoomControlsEnabled(true);
+                //toolbar de ir para desativado.
+                mMap.getUiSettings().setMapToolbarEnabled(false);
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                //Habilitar botao de compasso
+                mMap.getUiSettings().setCompassEnabled(true);
+
+                setarPosicaoInicial();
+
+                mMap.setBuildingsEnabled(true);
+                /*
+                3000 = milisegundos para exibição da animação do mapa.
+                 */
+                mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                    @Override
+                    public void onInfoWindowClick(Marker marker) {
+                    }
+                });
+            }
+    }
+
+    public void limparMarcadores() {
+        if (mMap != null) {
+            mMap.clear();
+        }
+    }
+
+    public void setarPosicaoInicial() {
+        /*
+                Localização padrao quando o app é aberto
+                 */
+            posicao = new LatLng(-19.923605, -43.992537);
+            BEARING = 309;
+            ZOOM = 13;
+                /*
+                zoom = zoom que será setado no mapa no incio da aplicação
+                tilt = angulo "3D" do mapa
+                 */
+        if (cp == null) {
+            CameraUpdate update = CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(posicao).zoom(ZOOM).bearing(BEARING).tilt(0).build());
+            mMap.animateCamera(update, 2000, null);
+            mMap.moveCamera(update);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(LOG, "LocationChanged!!");
+        localizacao = location;
+        minhaloc.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+        minhaloc.setSnippet("Precisão: " + (int) location.getAccuracy() + " metros");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            startLocationUpdate();
+        }
+        if (cp != null) {
+            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cp));
+            cp = null;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+        if(mMap != null) {
+            cp = mMap.getCameraPosition();
+        }
+
+        if (mGoogleApiClient != null) {
+            stopLocationUpdate();
+        }
+
+    }
+
+    private synchronized void callConnection() {
+        Log.i(LOG, "callConnection()");
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+
+    private void initLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    private void startLocationUpdate() {
+        initLocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+    private void stopLocationUpdate() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(LOG, "onConnected(" + bundle + ")");
+        Location l = LocationServices
+                .FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+        if (l != null) {
+            Log.i(LOG, "latitude: " + l.getLatitude());
+            Log.i(LOG, "longitude: " + l.getLongitude());
+        }
+
+        startLocationUpdate();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG, "onConnectionSuspended(" + i + ")");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(LOG, "onConnectionFailed(" + connectionResult + ")");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    public void MostrarDistancia(LatLng position, String nomMarker) {
+        Location temp = new Location("temp");
+        temp.setLatitude(position.latitude);
+        temp.setLongitude(position.longitude);
+        int distancia = distanciaAproximadaemMetros(temp);
+        if (distancia != -1) {
+            Toast toast = Toast.makeText(getActivity(), "Você está a aproximadamente " + distancia + " metros do marker " + nomMarker, Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+            Toast toast = Toast.makeText(getActivity(), "Não foi possível calcular sua distância até o marker " + nomMarker, Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    public int distanciaAproximadaemMetros(Location local) {
+        if(localizacao != null) {
+                return (int) localizacao.distanceTo(local);
+            } else {
+                return -1;
+            }
+
+    }
+}
